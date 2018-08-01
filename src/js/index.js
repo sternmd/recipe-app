81830ee7fdb3e78a3b9acb8788f71651c446cc3b
Maxwell Stern <maxwell.stern@makingwaves.com>
2018-08-01 16:48:11 -0500

    Making first API call.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,15 @@
 // Global app controller
-import x from './test';
-const y = 23;
-console.log(`I imported ${x} from another module and here is ${y}`);
+import axios from 'axios';
+
+async function getResults(query) {
+  const proxy = 'https://cors-anywhere.herokuapp.com/'
+  const key = 'ae0483932ce7c467c2f17050d06831dd';
+
+  try {
+      const result = await axios(`${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`);
+      const recipes = result.data.recipes;
+      console.log(recipes);
+  } catch(err) {
+      alert(err);
+    }
+}
