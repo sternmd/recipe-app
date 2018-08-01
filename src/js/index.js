8e2d7099cc8d8a9344244fb3260e835eded2ac2e
Maxwell Stern <maxwell.stern@makingwaves.com>
2018-08-01 17:27:31 -0500

    building the Search Controller.

--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,32 @@
-// Global app controller
-
 import Search from './models/Search';
+
+// Global state of the app
+const state = {};
+
+const controlSearch = async () => {
+  // 1) get query from the viewport
+  const query = 'pizza';
+
+  if (query) {
+      // 2) new search object and add it to state
+      state.search = new Search(query);
+
+      // 3) Prepare UI for results__btn
+
+      // 4) search for rescipes
+      await state.search.getResults();
+
+      // 5) Render results on UI
+      console.log(state.search.result);
+  }
+
+}
+
+document.querySelector('.search').addEventListener('submit', e => {
+  e.preventDefault();
+  controlSearch();
+});
 
 const search = new Search('pizza');
 console.log(search)
-
 search.getResults();
