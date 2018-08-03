960d48e4fa4cc637d6a2e37be6126661bb348ede
Maxwell Stern <stern.maxwell@gmail.com>
2018-08-03 15:42:41 -0500

    implement pagination.

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -8,6 +8,7 @@ export const clearInput = () => {
 
 export const clearResults = () => {
   elements.searchResultList.innerHTML = '';
+  elements.searchResultPages.innerHTML = '';
 };
 
 // reduce title size to one line
@@ -43,7 +44,46 @@ const renderRecipe = recipe => {
     elements.searchResultList.insertAdjacentHTML('beforeend', markup);
 };
 
+// type: prev or 'next'
+const createButton = (page, type) => `
+  <button class="btn-inline results__btn--${type}" data-goto=${type === 'prev' ? page - 1 : page + 1}>
+    <span>Page ${type === 'prev' ? page - 1 : page + 1}</span>
+    <svg class="search__icon">
+        <use href="img/icons.svg#icon-triangle-${type === 'prev' ? 'left' : 'right'}"></use>
+    </svg>
+  </button>
+`;
+
+const renderButtons = (page, numResults, resultsPerPage) => {
+  const pages = Math.ceil(numResults / resultsPerPage);
+
+  let button;
+
+  if (page === 1 && pages > 1) {
+    // only button to go to next page
+    button = createButton(page, 'next');
+  } else if (page < pages) {
+    // both buttons
+    button = `
+      ${createButton(page, 'prev')}
+      ${createButton(page, 'next')}
+    `;
+  }
+  else if  (page === pages && pages > 1) {
+    // only button to go to previous page
+    button = createButton(page, 'prev');
+  }
+
+  elements.searchResultPages.insertAdjacentHTML('afterbegin', button);
+};
+
 // loop through array
-export const renderResults = recipes => {
-  recipes.forEach(renderRecipe);
+export const renderResults = (recipes, page = 1, resultsPerPage = 10) => {
+  // render results of current page
+  const start = (page - 1) * resultsPerPage;
+  const end = page * resultsPerPage;
+  recipes.slice(start, end).forEach(renderRecipe);
+
+  // render pagination buttons
+  renderButtons(page, recipes.length, resultsPerPage);
 };
